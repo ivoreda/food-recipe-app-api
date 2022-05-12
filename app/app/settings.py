9593cff8488c08606ab99214f845e7cb067900c9
ivor edafe <ivoredafej@gmail.com>
2022-05-12 15:00:28 +0100

    Added endpoint to create and authenticate users

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
---
+++ app/user/tests/foobar.py
@@ -0,0 +1,7 @@
+def greeting(name: str):
+    name = 'ivor'
+    return 'hello' + name
+
+
+name = 'ivoe'
+greeting(name)
