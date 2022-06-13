0361ca716a4c776cdaf516b94ac634b164d9a702
ivor edafe <ivoredafej@gmail.com>
2022-06-13 03:58:28 +0100

    Added create recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,10 +38,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipes in the Database"""
     serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
-
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         """Retrieve the recipes for the authenticated user"""
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == "retrieve":
+            return serializers.RecipeDetailSerializer
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
